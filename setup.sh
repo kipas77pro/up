9b247202ed90f3bf89c9a42d4a3facf8c999a97f
kipas77pro <lahbaru930@gmail.com>
2025-05-20 11:49:20 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-#########################
-
 clear
+rm -rf setup.sh
+rm -rf /etc/xray/domain
+rm -rf /etc/v2ray/domain
+rm -rf /etc/xray/scdomain
+rm -rf /etc/v2ray/scdomain
+rm -rf /var/lib/ipvps.conf
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
+BRed='\e[1;31m'
+BGreen='\e[1;32m'
+BYellow='\e[1;35m'
+BBlue='\e[1;32m'
 NC='\e[0m'
 purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
 tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-echo -e "\033[0;37m Wellcome Script Rosi Center "
-echo -e " Tuk Infonya Silahkan Hubungi Admin"
-echo -e " Version MultiPort "
-echo -e "\033[0;36m By Rosi Center 081931472448 "
-echo -e "\033[0;32m"
 
-# ==========================================
-MYIP=$(wget -qO- icanhazip.com);
+
 clear
-clear
+# domain random
+CDN="https://raw.githubusercontent.com/Arya-Blitar22/auto/main/ssh"
 cd /root
 #System version number
 if [ "${EUID}" -ne 0 ]; then
@@ -48,45 +51,39 @@ touch /etc/xray/scdomain
 touch /etc/v2ray/scdomain
 
 
-echo -e "[ ${tyblue}NOTES${NC} ] Sebelum kita pergi.. "
-sleep 1
-echo -e "[ ${tyblue}NOTES${NC} ] Saya perlu memeriksa tajuk Anda terlebih dahulu .."
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Memeriksa header"
-sleep 1
+echo -e "[ ${BBlue}NOTES${NC} ] Sebelum kita pergi.. "
+sleep 0.5
+echo -e "[ ${BBlue}NOTES${NC} ] Saya perlu memeriksa header Anda terlebih dahulu..."
+sleep 0.5
+echo -e "[ ${BGreen}INFO${NC} ] Memeriksa header"
+sleep 0.5
 totet=`uname -r`
 REQUIRED_PKG="linux-headers-$totet"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "" = "$PKG_OK" ]; then
-  sleep 2
-  echo -e "[ ${yell}WARNING${NC} ] Coba pasang ..."
+  sleep 0.5
+  echo -e "[ ${BRed}WARNING${NC} ] Cobalah untuk menginstal ...."
   echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
   apt-get --yes install $REQUIRED_PKG
-  sleep 1
+  sleep 0.5
   echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] Jika kesalahan Anda perlu .. untuk melakukan ini"
-  sleep 1
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] If error you need.. to do this"
+  sleep 0.5
   echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 1. apt update -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 2. apt upgrade -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 3. apt dist-upgrade -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 4. reboot"
-  sleep 1
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] apt update && upgrade"
+  sleep 0.5
   echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] Setelah reboot"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] Kemudian jalankan skrip ini lagi"
-  echo -e "[ ${tyblue}NOTES${NC} ] jika kamu mengerti maka ketuk masuk sekarang"
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] Setelah ini"
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] Kemudian jalankan skrip ini lagi"
+  echo -e "[ ${BBlue}NOTES${NC} ] enter now"
   read
 else
-  echo -e "[ ${green}INFO${NC} ] Oke terpasang"
+  echo -e "[ ${BGreen}INFO${NC} ] Oke terpasang"
 fi
 
 ttet=`uname -r`
@@ -107,55 +104,89 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
-coreselect=''
-cat> /root/.profile << END
-# ~/.profile: executed by Bourne-compatible login shells.
-
-if [ "$BASH" ]; then
-  if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
-  fi
-fi
-
-mesg n || true
-clear
-END
-chmod 644 /root/.profile
-
-echo -e "[ ${green}INFO${NC} ] Mempersiapkan file instalasi"
+echo -e "[ ${BGreen}INFO${NC} ] Mempersiapkan file instalasi"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] Oke bagus...file instalasi sudah siap"
-sleep 2
-echo -ne "[ ${green}INFO${NC} ] Periksa izin : "
+echo -e "[ ${BGreen}INFO${NC} ] Oke bagus... file instalasi sudah siap"
+sleep 0.5
+echo -ne "[ ${BGreen}INFO${NC} ] Periksa izin : "
 
-mkdir -p /var/lib/SIJA >/dev/null 2>&1
-echo "IP=" >> /var/lib/SIJA/ipvps.conf
+echo -e "$BGreen Izin Diterima!$NC"
+sleep 2
+
+mkdir -p /var/lib/ >/dev/null 2>&1
+echo "IP=" >> /var/lib/ipvps.conf
 
 echo ""
-wget -q https://raw.githubusercontent.com/Panwas89/anjing/main/tools.sh;chmod +x tools.sh;./tools.sh
-rm tools.sh
 clear
-#install ssh ovpn
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green      Install SSH / WS               $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+    echo -e "$NC                 PENYIAPAN DOMAIN VPS     $NC"
+    echo -e "$BYellow----------------------------------------------------------$NC"
+    echo -e "$NC 1. Gunakan Domain Acak $NC"
+    echo -e "$NC 2. Gunakan Domain Sendiri $NC"
+    echo -e "$BYellow----------------------------------------------------------$NC"
+    read -rp " Pilih domain yang akan kamu pakai : " dns
+	if test $dns -eq 1; then
+    clear
+    apt install jq curl -y
+    wget -q -O /root/cf "${CDN}/cf" >/dev/null 2>&1
+    chmod +x /root/cf
+    bash /root/cf | tee /root/install.log
+    print_success "Domain Acak Done"
+	elif test $dns -eq 2; then
+    read -rp "Enter Your Domain : " dom
+    echo "$dom" > /root/scdomain
+	echo "$dom" > /etc/xray/scdomain
+	echo "$dom" > /etc/xray/domain
+	echo "$dom" > /etc/v2ray/domain
+	echo "$dom" > /root/domain
+    echo "IP=$dom" > /var/lib/ipvps.conf
+    else 
+    echo "Argumen Tidak Ditemukan"
+    exit 1
+    fi
+	echo -e "${BGreen}Selesai!${NC}"
+    sleep 2
+    clear
+    
+#install
+/etc/init.d/vnstat restart >/dev/null 2>&1
+wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc >/dev/null 2>&1 && make >/dev/null 2>&1 && make install >/dev/null 2>&1
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat >/dev/null 2>&1
+/etc/init.d/vnstat restart >/dev/null 2>&1
+rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
+rm -rf /root/vnstat-2.6 >/dev/null 2>&1
+
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Utama berhasil diinstal..."
+sleep 3
 clear
-wget https://raw.githubusercontent.com/Panwas89/anjing/main/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[42;1;37m           >>> Install Ssh Vpn <<<          \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 1
 wget https://raw.githubusercontent.com/kipas77pro/tunel/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-#Instal Xray
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          Install XRAY              $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-clear
-wget https://raw.githubusercontent.com/Panwas89/anjing/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget https://raw.githubusercontent.com/Panwas89/anjing/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
-#wget https://raw.githubusercontent.com/Panwas89/anjing/main/ssh/openvpn && chmod +x openvpn && ./openvpn
-wget https://raw.githubusercontent.com/Panwas89/anjing/main/ssh/vpn.sh && chmod +x vpn.sh && ./vpn.sh
-wget https://raw.githubusercontent.com/Panwas89/anjing/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
-clear
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[42;1;37m           >>> Install Vray <<<          \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 1
+wget https://raw.githubusercontent.com/Arya-Blitar22/bokep2/main/v1/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[42;1;37m           >>> Install Backup <<<          \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 1
+wget https://raw.githubusercontent.com/Arya-Blitar22/bokep/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[42;1;37m           >>> Install Wsoket <<<          \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 1
+wget https://raw.githubusercontent.com/Arya-Blitar22/mainan/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -178,10 +209,23 @@ fi
 if [ -f "/etc/afak.conf" ]; then
 rm /etc/afak.conf > /dev/null 2>&1
 fi
-if [ ! -f "/etc/log-create-user.log" ]; then
-echo "Log All Account " > /etc/log-create-user.log
+if [ ! -f "/etc/log-create-ssh.log" ]; then
+echo "Log SSH Account " > /etc/log-create-ssh.log
+fi
+if [ ! -f "/etc/log-create-vmess.log" ]; then
+echo "Log Vmess Account " > /etc/log-create-vmess.log
+fi
+if [ ! -f "/etc/log-create-vless.log" ]; then
+echo "Log Vless Account " > /etc/log-create-vless.log
+fi
+if [ ! -f "/etc/log-create-trojan.log" ]; then
+echo "Log Trojan Account " > /etc/log-create-trojan.log
+fi
+if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
+echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
 fi
 history -c
+serverV=$( curl -sS https://raw.githubusercontent.com/Arya-Blitar22/auto/main/menu/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
@@ -191,7 +235,8 @@ gg="PM"
 else
 gg="AM"
 fi
-curl -sS ifconfig.me > /etc/myipvps
+curl -sS ipv4.icanhazip.com > /etc/myipvps
+
 #install gotop
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
     gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
@@ -199,59 +244,41 @@ gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | 
     dpkg -i /tmp/gotop.deb >/dev/null 2>&1
     
 clear
-echo ""
-echo "Scprit Berhasil Di Install Sayank  !! "
-echo ""
-echo "===============-[ Script Created By Rosi Center ]-==============="
-echo ""
-echo "-----------------------------------------------------------------"
-echo ""
-echo ""
+echo "==================================================================" | tee -a log-install.txt
+echo " "
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH		: 22"  | tee -a log-install.txt
-echo "   - SSH Websocket	: 80, 8880, 2082, 8080" | tee -a log-install.txt
-echo "   - SSH SSL Websocket	: 443, 8443, 2096, 2083" | tee -a log-install.txt
-echo "   - Stunnel4		: 445, 777" | tee -a log-install.txt
-echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
-echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
-echo "   - Nginx		: 81" | tee -a log-install.txt
-echo "   - Vmess TLS		: 443, 8443, 2096, 2083" | tee -a log-install.txt
-echo "   - Vmess None TLS	: 80, 8880, 2082, 8080" | tee -a log-install.txt
-echo "   - Vless TLS		: 443" | tee -a log-install.txt
-echo "   - Vless None TLS	: 80, 8880, 2082, 8080" | tee -a log-install.txt
-echo "   - Trojan GRPC        : 443, 8443, 2096, 2083" | tee -a log-install.txt
-echo "   - Trojan WS		: 443, 8443, 2096, 2083" | tee -a log-install.txt
-echo "   - Trojan Go		: 2087" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban		: [ON]"  | tee -a log-install.txt
-echo "   - Dflate		: [ON]"  | tee -a log-install.txt
-echo "   - IPtables		: [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot        : [ON]"  | tee -a log-install.txt
-echo "   - IPv6	        : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On	: $aureb:00 $gg GMT +7" | tee -a log-install.txt
-echo "   - AutoKill Multi Login User" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Fully automatic script" | tee -a log-install.txt
-echo "   - VPS settings" | tee -a log-install.txt
-echo "   - Admin Control" | tee -a log-install.txt
-echo "   - Change port" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
+echo "   - SSH Websocket            : 80, 8880" | tee -a log-install.txt
+echo "   - SSH SSL Websocket        : 443, 8443" | tee -a log-install.txt
+echo "   - Stunnel4                 : 445, 447, 777" | tee -a log-install.txt
+echo "   - Dropbear                 : 109, 143" | tee -a log-install.txt
+echo "   - Badvpn                   : 7100-7900" | tee -a log-install.txt
+echo "   - Nginx                    : 81" | tee -a log-install.txt
+echo "   - Vmess WS TLS             : 443, 8443" | tee -a log-install.txt
+echo "   - Vmess WS none TLS        : 80, 8880" | tee -a log-install.txt
+echo "   - Vless WS none TLS        : 80, 8880" | tee -a log-install.txt
+echo "   - Vmess gRPC               : 443, 8443" | tee -a log-install.txt
 echo ""
-echo ""
-echo "-----------------------------------------------------------------"
-echo ""
-echo "===============-[ Script Created By Rosi Center ]-==============="
+echo "==================================================================" | tee -a log-install.txt
 echo -e ""
-echo -e " Wa me 081931472448 "
 echo ""
 echo "" | tee -a log-install.txt
 rm /root/setup.sh >/dev/null 2>&1
 rm /root/ins-xray.sh >/dev/null 2>&1
+rm /root/ssh-vpn.sh >/dev/null 2>&1
 rm /root/insshws.sh >/dev/null 2>&1
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
 echo -e "
 "
-read -n 1 -s -r -p "Klik Enter Auto Reboot"
+echo -ne "[ ${yell}PERINGATAN${NC} ] reboot sekarang ? (y/n)? "
+read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+exit 0
+else
 reboot
+fi
+
+
+
+
+
